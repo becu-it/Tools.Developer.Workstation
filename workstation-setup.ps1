358855690ab17073e0dff8e77e697402bdef370c
darsen <danijel.arsenovski@gmail.com>
2017-12-13 13:30:38 -0800

    install withouht permissions:

--- workstation-setup.ps1
+++ workstation-setup.ps1
@@ -1,6 +1,7 @@
 function ChocolateyInstall{
 	New-Item c:\helloVMExt.txt -type file
-	& @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+	iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) 
+	SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 }
 
 function InstallSoftware{
